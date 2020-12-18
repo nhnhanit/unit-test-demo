e5ef3a80f8bf94f79001f7964a179ef95c2f6ea4
Nguyen Hong Nhan <nhnhanit@gmail.com>
2020-12-18 22:53:00 +0700

    Validate Login Screen

---
+++ UnitTest-Demo/UIViewController+Extensions.swift
@@ -0,0 +1,26 @@
+//
+//  UIViewController+Extensions.swift
+//  UnitTest-Demo
+//
+//  Created by Nguyen Hong Nhan on 12/18/20.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    private func present(_ dismissableAlert: UIAlertController) {
+        let dismissAction = UIAlertAction(title: "Dismiss", style: .cancel)
+        dismissableAlert.addAction(dismissAction)
+        present(dismissableAlert, animated: true)
+    }
+    
+    func presentAlert(with message: String) {
+        let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+        present(alert)
+    }
+    
+    func present(_ error: Error) {
+        presentAlert(with: error.localizedDescription)
+    }
+}
---
+++ UnitTest-Demo/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  UnitTest-Demo
+//
+//  Created by Nguyen Hong Nhan on 12/18/20.
+//
+
+import Foundation
+
+struct User {
+    let username: String
+    let password: String
+}
