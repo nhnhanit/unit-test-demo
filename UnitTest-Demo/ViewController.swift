44bf8fad38558a324ec9294a89ec959866a90e65
Nguyen Hong Nhan <nhnhanit@gmail.com>
2020-12-18 23:08:49 +0700

    ValidationService Struct

--- UnitTest-Demo/ViewController.swift
+++ UnitTest-Demo/ViewController.swift
@@ -14,6 +14,18 @@ class ViewController: UIViewController {
     
     private let dummyDatabase = [User(username: "kilo loco", password: "password1")]
     
+    private let validation: ValidationService
+    
+    init(validation: ValidationService) {
+        self.validation = validation
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        self.validation = ValidationService()
+        super.init(coder: coder)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -21,20 +33,25 @@ class ViewController: UIViewController {
     
     @IBAction func didTapLoginButton(_ sender: Any) {
         do {
-            guard
-                let username = usernameTextField.text,
-                let password = passwordTextField.text
-            else { throw ValidationError.invalidValue }
             
-            guard username.count > 3 else { throw ValidationError.usernameTooShort }
-            guard username.count < 20 else { throw ValidationError.usernameTooLong }
-            guard password.count >= 8 else { throw ValidationError.passwordTooShort }
-            guard password.count < 20 else { throw ValidationError.passwordTooLong }
+            let username = try validation.validateUsername(usernameTextField.text)
+            let password = try validation.validatePassword(passwordTextField.text)
+            
+            
+//            guard
+//                let username = usernameTextField.text,
+//                let password = passwordTextField.text
+//            else { throw ValidationError.invalidValue }
+//
+//            guard username.count > 3 else { throw ValidationError.usernameTooShort }
+//            guard username.count < 20 else { throw ValidationError.usernameTooLong }
+//            guard password.count >= 8 else { throw ValidationError.passwordTooShort }
+//            guard password.count < 20 else { throw ValidationError.passwordTooLong }
             
             
             // Login to database...
             if let user = dummyDatabase.first(where: { user in
-                user.username == username && user.password == user.password
+                user.username == username && user.password == password
             }) {
                 presentAlert(with: "You successfully logged in as \(user.username)")
                 
@@ -49,29 +66,6 @@ class ViewController: UIViewController {
 }
 
 extension ViewController {
-    enum ValidationError: LocalizedError {
-        case invalidValue
-        case passwordTooLong
-        case passwordTooShort
-        case usernameTooLong
-        case usernameTooShort
-        
-        var errorDescription: String? {
-            switch self {
-            case .invalidValue:
-                return "You have entered an invalid value."
-            case .passwordTooLong:
-                return "Your password is too long."
-            case .passwordTooShort:
-                return "Your password is too short."
-            case .usernameTooLong:
-                return "Your username is too long."
-            case .usernameTooShort:
-                return "Your username is too short."
-            }
-        }
-    }
-    
     enum LoginError: LocalizedError {
         case invalidCredentials
         
